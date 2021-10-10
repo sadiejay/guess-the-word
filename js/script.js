c1fc2ff4f57065ad660279f4d12aa6f631a255a1
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-10-10 16:37:26 -0500

    Defines  global variables

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// The unordered list where the player’s guessed letters will appear.
+var guessList = document.querySelector('guessed-letters');
+
+// The button with the text “Guess!” in it.\
+var guessButton = document.querySelector('.guess');
+
+// The text input where the player will guess a letter.
+var guessLetter = document.querySelector('.letter');
+
+// The empty paragraph where the word in progress will appear.
+var wordIP = document.querySelector('.word-in-progress');
+
+// The paragraph where the remaining guesses will display.
+var guessRemainingPara = document.querySelector('.remaining');
+
+// The span inside the paragraph where the remaining guesses will display.
+var guessRemainingSpan = document.querySelector('.remaining span');
+
+// The empty paragraph where messages will appear when the player guesses a letter.
+var message = document.querySelector('.message');
+
+// The hidden button that will appear prompting the player to play again.
+var playAgain= document.querySelector('.play-again');
+
+// Magnolia is your starting word to test out the game 
+var word = 'magnolia';
